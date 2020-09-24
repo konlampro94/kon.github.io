867fa043f61793bbd09af2216810048a675ddc1e
konlampro94 <konlampro94@gmail.com>
2020-09-24 23:54:30 +0300

    events 8

--- event.js
+++ event.js
@@ -2,4 +2,4 @@ var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     var form_btn = document.getElementById("form_btn");
     form_btn.style.background = "green";
-}, true)();
+}, true);
